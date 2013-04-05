925f45a0ea662630b385e2da185979f77185ab6e
Cody Barron <cbarron1@nd.edu>
2013-04-05 10:46:06 -0400

    Broke the title screen and need to fix it

--- CApp_OnTitle.cpp
+++ CApp_OnTitle.cpp
@@ -1,46 +1,63 @@
 #include "CApp.h"
+#include "CSurface.h"
 
 int CApp::OnTitle() {
-	SDL_Surface* test = NULL;
-	SDL_Surface* screen = NULL;
 	
+	SDL_Surface* title;
+	if ((title = CSurface::OnLoad("./images/titleScreen/TitleScreen.png")) == false) return -1;
+	SDL_Surface* arrow;
+	if ((arrow = CSurface::OnLoad("./images/titleScreen/arrow.png")) == false) return -1;
+
+
+	SDL_Surface* screen = NULL;	
 	screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
 	
-	test = IMG_Load("images/titleScreen/TitleScreen.png");
-	
-	SDL_BlitSurface(test, NULL, screen, NULL);
+	CSurface::OnDraw(screen, title, 0,0);
 	
 	SDL_Flip(screen);
 	
 	SDL_Event event;
 	bool titleRunning = true;
+	bool choice1 = true;
+	bool choice2 = false;
 	
 	while (titleRunning) {
 		while (SDL_PollEvent(&event)) {
 			switch(event.type) {
 				case SDL_KEYDOWN:
 					switch(event.key.keysym.sym) {
-						case SDLK_1:
-							metroid = true;
-							castlevania = false;
-							titleRunning = false;
+						case SDLK_UP:
+							if (choice1) {
+									
+							}	
+							else if (choice2) {
+								
+							}	
 							break;
-						case SDLK_2:
-							metroid = false;
-							castlevania = true;
-							titleRunning = false;
+						case SDLK_DOWN:
+
+							break;
+						case SDLK_RETURN:
+							if (choice1) { 
+								metroid=true;
+								castlevania=false;
+								titleRunning=false;
+							} else { 
+								castlevania=true;
+								metroid=false;
+								titleRunning=false;
+							}
 							break;
 						default:
-							metroid = true;
-							castlevania = false;
-							titleRunning = false;
+							break;	
 					}	
 					break;
 			}	
 		}	
 	}	
 	
-	SDL_FreeSurface(test);
+	SDL_FreeSurface(title);
+	SDL_FreeSurface(arrow);
 	
 	SDL_Quit();
 	
