b05fae36d8d1ac81f6e4b79ea0ab0149f672a569
Andres Martin <amarti36@nd.edu>
2013-04-22 10:21:53 -0400

    added shooting animations for samus and fixed her moonwalking when pointing diagonally and running left

--- CApp_OnEvent.cpp
+++ CApp_OnEvent.cpp
@@ -67,6 +67,7 @@ void CApp::OnKeyDown(SDLKey sym, SDLMod mod, Uint16 unicode) {
 				if (!Samus.morphBall) //as long as she is not in a ball, she can shoot
 				{
 					SDL_EnableKeyRepeat(0, 10000);
+					Samus.shotTimer = 0;
 					CSoundBank::SoundControl.Play(-1, attackSound, 0);
 					CShot* Bullet = new CShot;
 					Bullet->OnLoad("./images/shot.png", 8, 8, 0);
